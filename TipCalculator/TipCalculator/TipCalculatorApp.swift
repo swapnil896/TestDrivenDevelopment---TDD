f8b78aecb6e36d74f41be7cd858ae115df88c985
Swapnil Magar <magarswapnil896@gmail.com>
2025-11-11 20:26:56 +0530

    Initial commit

---
+++ TipCalculator/TipCalculator/TipCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipCalculatorApp.swift
+//  TipCalculator
+//
+//  Created by Pranav Manikpure on 11/11/25.
+//
+
+import SwiftUI
+
+@main
+struct TipCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

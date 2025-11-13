baa76ffb650bf1e30767972d48ad6aba76cf2163
Swapnil Magar <magarswapnil896@gmail.com>
2025-11-13 09:39:54 +0530

    Mocking Demo

---
+++ MockingDemo/MockingDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MockingDemo
+//
+//  Created by Pranav Manikpure on 13/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MockingDemo/MockingDemo/MockingDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MockingDemoApp.swift
+//  MockingDemo
+//
+//  Created by Pranav Manikpure on 13/11/25.
+//
+
+import SwiftUI
+
+@main
+struct MockingDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
